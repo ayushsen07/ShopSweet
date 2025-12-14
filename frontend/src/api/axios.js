4d2b90134b8e6b4f4060e4079677e571acf895a5
Ayush sen <senayush920@gmail.com>
2025-12-14 23:12:16 +0530

    fix: update axios base URL to production endpoint

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create an instance of axios
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api', // Backend URL
+  baseURL: 'https://shopsweet.onrender.com/api', // Backend URL
   headers: {
     'Content-Type': 'application/json',
   },
